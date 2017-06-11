30a0f297b309fc422aae84a187cf21c25da18ce6
Piotr Barejko <bareya@users.noreply.github.com>
2017-06-11 12:47:06 -0700

    Initial (#1)
    * remove module dependency
    
    * remove modules
    
    * adding FindHoudini
    
    * Houdini find added
    
    * Working version

--- src/GEO_BifrostIOTranslator.h
+++ src/GEO_BifrostIOTranslator.h
@@ -3,8 +3,17 @@
 class GEO_BifrostIOTranslator : public GEO_IOTranslator
 {
 public:
-	GEO_BifrostIOTranslator(){}
-	virtual ~GEO_BifrostIOTranslator(){}
+	GEO_BifrostIOTranslator();
+	GEO_BifrostIOTranslator(const GEO_BifrostIOTranslator& other);
+	virtual ~GEO_BifrostIOTranslator();
 
-	virtual GA_Detail::IOStatus fileLoad (GEO_Detail *gdp, UT_IStream &is, bool ate_magic){}
+	virtual GEO_IOTranslator* duplicate() const;
+	virtual const char*	formatName() const;
+	virtual int	checkExtension(const char *name);
+
+	virtual int	checkMagicNumber(unsigned magic);
+
+	virtual GA_Detail::IOStatus fileLoad(GEO_Detail *gdp, UT_IStream &is, bool ate_magic);
+	virtual GA_Detail::IOStatus fileSave(const GEO_Detail *gdp, std::ostream &os);
 };
+
